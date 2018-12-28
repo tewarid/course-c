d6376ccfcb0b201bdaf484938287d74dd6736e11
Devendra Tewari <devendra.tewari@gmail.com>
2018-12-28 16:16:53 -0300

    force InlineMath filter

--- readme.md
+++ readme.md
@@ -12,11 +12,3 @@ I suggest using the slides in the following order
 8. [Input and Output](input-output.md)
 
 Presentation views rendered using pandoc are available in [reveal.js](slides.html) and [PowerPoint](slides.pptx) formats.
-
-## Known Issues
-
-- The course mentions Windows Subsystem for Unix (SFU) which is now defunct
-
-- Due to differences in markdown syntax supported by pandoc and kramdown, static HTML rendered by GitHub Pages/Jekyll has some issues
-
-  - Inline math is not rendered properly because kramdown fails to recognize single dollar sign delimiter `$...$` supported by pandoc
--- run-pandoc.sh
+++ run-pandoc.sh
@@ -1,5 +1,5 @@
 files=(front-matter.md objective.md introduction.md types-operators-expressions.md control-flow.md functions-advanced-program-structure.md pointers-arrays.md structures.md input-output.md)
 # create standalone revealjs presentation
-pandoc --slide-level=2 -o slides.html -t revealjs -s -V minScale=0.2 -V maxScale=1.5 -V width=\"100%\" -V height=\"150%\" -V margin=0.05 ${files[@]}
+pandoc --lua-filter filter.lua --slide-level=2 -o slides.html -t revealjs -s -V minScale=0.2 -V maxScale=1.5 -V width=\"100%\" -V height=\"150%\" -V margin=0.05 ${files[@]}
 # create PowerPoint
-pandoc --slide-level=2 -o slides.pptx ${files[@]}
+pandoc --lua-filter filter.lua --slide-level=2 -o slides.pptx ${files[@]}
